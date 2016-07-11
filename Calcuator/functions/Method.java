cdff8a84589146831a07116b71517761269554bb
ryandw11 <ryandw02@gmail.com>
2016-07-11 15:57:51 -0700

    Create Method.java

---
+++ Calcuator/functions/Method.java
@@ -0,0 +1,29 @@
+package functions;
+
+public class Method {
+		
+	public static double add(double num1, double num2){
+		double output;
+		output = num1 + num2;
+	return output;
+	}
+	
+	public static double minus(double num1, double num2){
+		double output;
+		output = num1 - num2;
+	return output;
+	}
+	
+	public static double multi(double num1, double num2){
+		double output;
+		output = num1 * num2;
+	return output;
+	}
+	
+	public static double divide(double num1, double num2){
+		double output;
+		output = num1 / num2;
+	return output;
+	}
+
+}
